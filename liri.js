b9aa253cdb92968fab52bf74fa25bdad5f224e49
Carlos Ponce <jcponce510@gmail.com>
2019-03-20 23:00:02 -0700

    Add concert-this functionality.

--- liri.js
+++ liri.js
@@ -4,6 +4,17 @@ require("dotenv").config();
 // Import keys.js.
 var keys = require("./keys.js");
 
+// Grab axios package.
+var axios = require("axios");
+var artist = process.argv[2];
+var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+// axios.get() function takes a URL and returns a promise.
+axios.get(queryURL).then(function(response) {
+  console.log(response.data.venue.name \n response.data.venue.city + ", " +
+    response.data.venue.region \n response.data.datetime)
+});
+
 // Access keys info.
 var spotify = new Spotify(keys.spotify);
 
@@ -13,9 +24,10 @@ var spotify = new Spotify(keys.spotify);
 * movie-this
 * do-what-it-says */
 
-/* What Each Command Should Do
+// What Each Command Should Do
 
-* node liri.js concert-this <artist/band name here>
+
+/* node liri.js concert-this <artist/band name here>
 
 This will search the Bands in Town Artist Events API
 ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
@@ -26,9 +38,12 @@ Name of the venue
 
 Venue location
 
-Date of the Event (use moment to format this as "MM/DD/YYYY")
+Date of the Event (use moment to format this as "MM/DD/YYYY") */
 
-* node liri.js spotify-this-song '<song name here>'
+
+
+
+/* node liri.js spotify-this-song '<song name here>'
 
 This will show the following information about the song in your terminal/bash
 window
@@ -62,9 +77,12 @@ like for these fields. When finished, click the "complete" button.
 
 Step Four: On the next screen, scroll down to where you see your client id and
 client secret. Copy these values down somewhere, you'll need them to use the
-Spotify API and the node-spotify-api package.
+Spotify API and the node-spotify-api package. */
 
-* node liri.js movie-this '<movie name here>'
+
+
+
+/* node liri.js movie-this '<movie name here>'
 
 This will output the following information to your terminal/bash window:
   * Title of the movie.
@@ -80,9 +98,12 @@ If the user doesn't type a movie in, the program will output data for the movie
 'Mr. Nobody.'
 
 You'll use the axios package to retrieve data from the OMDB API. Like all of the
-in-class activities, the OMDB API requires an API key. You may use trilogy.
+in-class activities, the OMDB API requires an API key. You may use trilogy. */
 
-* node liri.js do-what-it-says
+
+
+
+/* node liri.js do-what-it-says
 
 Using the fs Node package, LIRI will take the text inside of random.txt and then
 use it to call one of LIRI's commands.
